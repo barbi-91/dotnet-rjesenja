5b163c91f417540aedcb4a256a00282efbffbe3d
Barby <barbara.erdec@gmail.com>
2022-12-18 19:48:28 +0100

    Poglavlje varijabli - zadatak 1 rjesenje

--- Modul 1/00 - Variables/001_exercise/Program.cs
+++ Modul 1/00 - Variables/001_exercise/Program.cs
@@ -1,9 +1,14 @@
 ﻿/*
- * With the help of basic variables and the Console.ReadLine() command save two values, then assign variable "a" the entered value of variable "b" and assign variable "b" the entered value of variable "a"
+ * With the help of basic variables 
+ * and the Console.ReadLine() command 
+ * save two values, 
+ * then assign variable "a" the entered value of variable "b" 
+ * and assign variable "b" the entered value of variable "a"
  */
 
 // Here we have basic logic to insert data
-// (in this example input validation is not required, the user is expected to enter an integer values)
+// (in this example input validation is not required, the user is
+// expected to enter an integer values)
 Console.Write("Enter a value for variable \"a\": ");
 int a = int.Parse(Console.ReadLine());
 Console.Write("Enter a value for variable \"b\": ");
@@ -13,7 +18,9 @@ int b = int.Parse(Console.ReadLine());
 Console.WriteLine("\n\nBefore:\na = {0}\nb = {1}", a, b);
 
 // Insert your code logic here to replace values between variables
-
+int temp = a;
+a = b;
+b = temp;
 Console.WriteLine("\nAfter:\na = {0}\nb = {1}", a, b);
 
 
