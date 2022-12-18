fb948e653d31daeaa1cfd422357da78af54401a3
Barby <barbara.erdec@gmail.com>
2022-12-18 21:35:00 +0100

    Poglavlje Loops - zadatak 7 rjesenje

--- Modul 1/02 - Loops/007_exercise/Program.cs
+++ Modul 1/02 - Loops/007_exercise/Program.cs
@@ -2,13 +2,30 @@
  * Using a foreach loop find the number of occurrences of a character in the sequence
  */
 
-
-string sentence = "A monkey stole a banana, climbed a tree and ate it.";
-
 // Insert your code logic below using foreach loop
 // ==================================================================
 
 
+char[] array_of_characters;
+int length;
+string sentence = "A monkey stole a banana, climbed a tree and ate it.";
+length = sentence.Length;
+array_of_characters = sentence.ToCharArray(0, length);
+Dictionary<char, int> dictionaryChar = new Dictionary<char, int>();
+foreach (char c in array_of_characters)
+{
+    if (dictionaryChar.ContainsKey(c))
+    {
+        dictionaryChar[c]++;
+    }
+    else
+    {
+        dictionaryChar.Add(c, 1);
+    }
+}
+
+foreach (KeyValuePair<char, int> pair in dictionaryChar)
+    Console.WriteLine($"{pair.Key}:{pair.Value}");
 
 // This is only here to stop the program before exiting
 Console.ReadKey();
