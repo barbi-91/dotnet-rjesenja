c0eccf29867f7ad816c0e5593cc0a45109f7c0e5
Barby <barbara.erdec@gmail.com>
2022-12-18 20:25:53 +0100

    Poglavlje Conditional statements - zadatak 1 rjesenje

--- Modul 1/01 - Conditional statements/001_exercise/Program.cs
+++ Modul 1/01 - Conditional statements/001_exercise/Program.cs
@@ -2,7 +2,9 @@
  * Using If statement write a simple program to find eligibility for admission to professional studies
  */
 
-int physics, chemistry, mathematics;
+int physics, chemistry, mathematics, total, totalMathPhys;
+bool flag = true;
+
 
 // The rules for your conditions can be found in the next sentences
 Console.Write("The candidate can enroll in the study program only if he meets the following criteria: \n");
@@ -21,13 +23,62 @@ Console.Write("Enter the points obtained in Chemistry (0-100):");
 chemistry = Convert.ToInt32(Console.ReadLine());
 Console.Write("Enter the points obtained in Mathematics (0-100):");
 mathematics = Convert.ToInt32(Console.ReadLine());
-
+Console.WriteLine();
 Console.Write("Total number of points in Mathematics, Physics and Chemistry: {0}\n", mathematics + physics + chemistry);
 Console.Write("Total number of points in Mathematics and Physics: {0}\n", mathematics + physics);
 Console.WriteLine();
 
-
 // Insert your code logic below using combination of if, else if and else statements
+
+total = mathematics + physics + chemistry;
+totalMathPhys = mathematics + physics;
+
+if (total >= 180 || totalMathPhys >= 140)
+{
+    if (mathematics >= 65 && mathematics <= 100)
+    {
+        Console.WriteLine($"Mathematics: Pass, {mathematics} points ");
+    }
+    else
+    {
+        Console.WriteLine("Exam Mathematics failure");
+        flag = false;
+    }
+    if (physics >= 55 && physics <= 100)
+    {
+        Console.WriteLine($"Physics: Pass, {physics} points ");
+    }
+    else
+    {
+        Console.WriteLine("Exam Physics failure");
+        flag = false;
+    }
+    if (chemistry >= 50 && chemistry <= 100)
+    {
+        Console.WriteLine($"Chemistry: Pass, {chemistry} points ");
+    }
+    else
+    {
+        Console.WriteLine("Exam Chemistry failure");
+        flag = false;
+    }
+    if (flag == false)
+    {
+        Console.WriteLine($"If one of exam does not have critera points,regardless of the total {total}, this is not pass.");
+        Console.WriteLine("Candidat do not have the following criteria:");
+    }
+    else
+    {
+        Console.WriteLine("Candidat have the following criteria:  The candidate can enroll in the study program");
+    }
+}
+else
+{
+    Console.WriteLine("Candidat do not have the following criteria: ");
+}
+
+
+
 
 
 
