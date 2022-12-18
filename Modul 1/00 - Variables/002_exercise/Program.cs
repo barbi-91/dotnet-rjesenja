4323a3568f2f30af963b2a9a12cf08322b0d73f8
Barby <barbara.erdec@gmail.com>
2022-12-18 20:04:18 +0100

    Poglavlje varijabli - zadatak 2 rjesenje

--- Modul 1/00 - Variables/002_exercise/Program.cs
+++ Modul 1/00 - Variables/002_exercise/Program.cs
@@ -1,5 +1,8 @@
 ﻿/*
- * Using basic variables and arithmetic operators write a simple program that takes distance and time as input
+ * Using basic variables and arithmetic operators write a simple program 
+ * that takes 
+ * distance and time 
+ * as inputs
  * and displays speed km/h and mph.
  */
 
@@ -7,9 +10,11 @@ float distance, hour, minutes, seconds;
 float kilometres_per_hour = 0.0F, miles_per_hour = 0.0F;
 
 // Here we have basic logic to insert data
-// (in this example input validation is not required, the user is expected to enter an integer or float value type)
+// (in this example input validation is not required,
+// the user is expected to enter an integer or float value type)
 Console.Write("Enter the distance (meters): ");
 distance = Convert.ToSingle(Console.ReadLine());
+Console.WriteLine();
 Console.Write("Enter the time (hour): ");
 hour = Convert.ToSingle(Console.ReadLine());
 Console.Write("Enter the time (minutes): ");
@@ -18,10 +23,10 @@ Console.Write("Enter the time (seconds): ");
 seconds = Convert.ToSingle(Console.ReadLine());
 
 
-// Insert your code logic here to calculate speed in kilometers per hour and miles per hour
-
-
-
+// Insert your code logic here to calculate speed in kilometers per hour
+kilometres_per_hour = (distance / 1000) / (hour + (minutes / 60) + (seconds / 3600));
+// and miles per hour
+miles_per_hour = kilometres_per_hour * 0.62142F;
 // Here we have data output
 Console.WriteLine("Your speed in \"km/h\" is {0}", kilometres_per_hour);
 Console.WriteLine("Your speed in \"mph\" is {0}", miles_per_hour);
