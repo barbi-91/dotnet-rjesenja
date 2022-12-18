1886ba11de03da83bbb03d1555fe8cad5c24e1aa
Barby <barbara.erdec@gmail.com>
2022-12-18 20:47:28 +0100

    Poglavlje Conditional statements - zadatak 2 rjesenje"

--- Modul 1/01 - Conditional statements/002_exercise/Program.cs
+++ Modul 1/01 - Conditional statements/002_exercise/Program.cs
@@ -1,10 +1,9 @@
 ﻿/*
  * Using Switch statement write a simple menu-driven program to perform a calculation of two numbers.
  */
-
 using System;
 
-int first_number, second_number, select_option;
+int first_number, second_number, select_option, total;
 
 // Here we have basic logic to insert data
 // (in this example input validation is not required, the user is expected to enter an integer values)
@@ -19,8 +18,31 @@ Console.Write("\nSelect one option by number: ");
 select_option = Convert.ToInt32(Console.ReadLine());
 
 // Insert your code logic below using switch expression with case, don't forget about break and default keywords
-
-
-
+switch (select_option)
+{
+    case 1:
+        Console.WriteLine("Addition");
+        total = first_number + second_number;
+        Console.WriteLine($"Total:  { total}");
+        break;
+    case 2:
+        Console.WriteLine("Deduction");
+        total = first_number - second_number;
+        Console.WriteLine($"Total:  { total}");
+        break;
+    case 3:
+        Console.WriteLine("Multiplication");
+        total = first_number * second_number;
+        Console.WriteLine($"Total:  { total}");
+        break;
+    case 4:
+        Console.WriteLine("Division");
+        total = first_number / second_number;
+        Console.WriteLine($"Total:  { total}");
+        break;
+    default:
+        Console.WriteLine("Exit the program");
+        break;
+}
 // This is only here to stop the program before exiting
 Console.ReadKey();
