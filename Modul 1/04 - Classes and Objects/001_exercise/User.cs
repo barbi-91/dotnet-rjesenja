e6807be390823ce56c0772f9bbe7f1906c05d350
Barby <barbara.erdec@gmail.com>
2022-12-26 11:20:54 +0100

    Poglavlje Classes and Objects- zadatak 1 rjesenje

---
+++ Modul 1/04 - Classes and Objects/001_exercise/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _001_exercise
+{
+    internal class User
+    {
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public int Age { get; set; }
+        public string Address { get; set; }
+        public int Phone { get; set; }
+
+    }
+}
