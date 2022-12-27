ebc6aaa44bce942188db3b642c0f06e75a1a3919
Barby <barbara.erdec@gmail.com>
2022-12-27 15:43:19 +0100

    Poglavlje Classes and Objects- zadatak 4 rjesenje

---
+++ Modul 1/04 - Classes and Objects/004_exercise/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _004_exercise
+{
+    internal class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string TaxPayerID { get; set; }
+
+        public Person()
+        {
+            FirstName = String.Empty;
+            LastName = String.Empty;    
+            DateOfBirth = DateTime.Now;
+            TaxPayerID = String.Empty;
+        }
+    }
+}
--- Modul 1/04 - Classes and Objects/004_exercise/Program.cs
+++ Modul 1/04 - Classes and Objects/004_exercise/Program.cs
@@ -15,8 +15,17 @@ internal class Program
 
         // Inside Person class create default constructor that will set all string value types to String.Empty, and DateTime value to DateTime.Now
         // Create object of the Person class and print defalut values
+        Person person = new Person();
+        Console.WriteLine("constructor - default");
+        Console.WriteLine("First name: " + person.FirstName  + " \n Last name: " + person.LastName + " \n Date of Birth: " + person.DateOfBirth + "\n Tax payer ID: " + person.TaxPayerID);
+        Console.WriteLine("*********");
         // Next, set an arbitrary values to your object and print them to console to see the difference in values
-
+        person.FirstName = "Ana";
+        person.LastName = "Programeric";
+        person.DateOfBirth = new DateTime(2008, 12, 25);
+        person.TaxPayerID = "34567";
+        Console.WriteLine("constructor - assign values");
+        Console.WriteLine("First name: " + person.FirstName + " \n Last name: " + person.LastName + " \n Date of Birth: " + person.DateOfBirth + "\n Tax payer ID: " + person.TaxPayerID);
 
         // This is only here to stop the program before exiting
         Console.ReadKey();
