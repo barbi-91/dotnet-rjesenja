fbff9caa69325397919d5223b14da98c343a2bf6
Barby <barbara.erdec.golovic@gmail.com>
2023-03-01 10:26:24 +0100

    Excersice  2 - 01-Asp.NET MVC

---
+++ 05 Modul/01 - ASP.NET MVC (CRUD)/Exercise 2/Controllers/CarController.cs
@@ -0,0 +1,113 @@
+﻿using Exercise_2.Models;
+using Exercise_2.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Exercise_2.Controllers
+{
+    public class CarController : Controller
+    {
+        DemoClass _dataCar;
+
+        public CarController()
+        {
+            _dataCar = new DemoClass();
+        }
+
+
+
+
+
+        // GET: CarController
+        public ActionResult Index()
+        {
+            List<Car> carsList = _dataCar.GetData().OrderBy(a =>a.Id).ToList();
+            return View(carsList);
+        }
+
+
+
+
+        // GET: CarController/Details/5
+        public ActionResult Details(int id)
+        {
+            var car = _dataCar.GetById(id);
+            return View(car);
+        }
+
+
+
+
+        // GET: CarController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CarController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create (Car createCar)
+        {
+            try
+            {
+                _dataCar.CreateData(createCar);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+
+
+
+
+        // GET: CarController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            Car carbyId = _dataCar.GetById(id);
+            return View(carbyId);
+        }
+
+        // POST: CarController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Car carObj)
+        {
+            try
+            {
+                _dataCar.UpdateCar(carObj);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CarController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            Car car = _dataCar.GetById(id);
+            return View(car);
+        }
+
+        // POST: CarController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                _dataCar.DeleteCar(id);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ 05 Modul/01 - ASP.NET MVC (CRUD)/Exercise 2/Repository/DemoClass.cs
@@ -0,0 +1,70 @@
+﻿using Exercise_2.Models;
+
+namespace Exercise_2.Repository
+{
+    public class DemoClass
+    {
+        private static List<Car> _cars;
+
+        public DemoClass()
+        {
+            if (_cars == null)
+            {
+                _cars = new List<Car>();
+                DataCar();
+            }
+        }
+
+        public void DataCar()
+        {
+            _cars = new List<Car>
+            { 
+            new Car {Id = 11, Make = "Toyota", Model = "Corolla", Year = 2020, Color = "Gray", Price = 25000},
+            new Car {Id = 12, Make = "Honda", Model = "Civic", Year = 2021, Color = "Red", Price = 28000},
+            new Car {Id = 15, Make = "Ford", Model = "F-150", Year = 2018, Color = "Blue", Price = 35000},
+            new Car {Id = 16, Make = "BMW", Model = "X5", Year = 2019, Color = "Black", Price = 50000},
+            new Car {Id = 20, Make = "Chevrolet", Model = "Camaro", Year = 2016, Color = "Yellow", Price = 42000},
+            };
+        }
+
+        //Metoda za dohvacanje liste i svih podataka uz njoj
+        public List<Car> GetData()
+        {
+            return _cars;
+        }
+
+        // Metoda za kreiranje novog podatka
+        public void CreateData(Car createCar)
+        {
+            _cars.Add(createCar);
+        }
+
+        //metoda za dohvacanje specificnog podatka
+        public Car GetById(int id)
+        {
+            var carById = _cars.FirstOrDefault(x => x.Id == id);
+            
+            return carById;
+        }
+
+        internal void UpdateCar(Car carObj)
+        {
+            var carToUpdate = _cars.FirstOrDefault(c => c.Id == carObj.Id);
+
+            if (carToUpdate != null)
+            {
+                carToUpdate.Price = carObj.Price;
+                carToUpdate.Year = carObj.Year;
+                carToUpdate.Make = carObj.Make;
+                carToUpdate.Model = carObj.Model;
+                carToUpdate.Color = carObj.Color;
+            }
+        }
+
+        internal void DeleteCar(int id)
+        {
+            Car deleteCar = _cars.FirstOrDefault(c => c.Id == id);
+            _cars.Remove(deleteCar);
+        }
+    }
+}
