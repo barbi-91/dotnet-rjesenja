c5b953337640fd64717f5e7f18def26685e0060b
Barby <barbara.erdec.golovic@gmail.com>
2023-03-24 13:44:11 +0100

    Exsercise01-Ef_code_first

---
+++ 08 Modul/00 - EF Code First/Exsercise01/Models/Book.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Exsercise01.Models
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        [Column(TypeName="nvarchar")]
+        public string Title { get; set; }
+
+
+        [Required]
+        [StringLength(100)]
+        [Column(TypeName = "nvarchar")]
+        public string Author { get; set; }
+
+
+        [Required]
+        [Column(TypeName = "date")]
+        public DateTime PublicationDate { get; set; }
+
+
+        [Required]
+        [StringLength(50)]
+        [Column(TypeName = "nvarchar")]
+        public string Genere { get; set; }
+
+
+    }
+}
---
+++ 08 Modul/00 - EF Code First/Exsercise01/Models/BookDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Exsercise01.Models
+{
+    public class BookDbContext : DbContext
+    {
+        public DbSet<Book> Books { get; set;}
+        public BookDbContext(DbContextOptions<BookDbContext> options) : base(options) { }
+    }
+}
